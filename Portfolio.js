ff74fd622c93ed3bb8262a95621d9d096a609dea
SATHIYAPRIYAN <159710724+Sathiyapriyanthangavel@users.noreply.github.com>
2024-08-09 18:37:37 +0530

    Delete Portfolio.js

--- Portfolio.js
+++
@@ -1,8 +0,0 @@
-// JavaScript for Hamburger Menu Toggle
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
-
