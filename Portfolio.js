4929493c72a18dc4f46464be6207b237ae99f320
SATHIYAPRIYAN <159710724+Sathiyapriyanthangavel@users.noreply.github.com>
2024-08-09 18:27:29 +0530

    Add files via upload
    Personal_Portfolio

---
+++ Portfolio.js
@@ -0,0 +1,8 @@
+// JavaScript for Hamburger Menu Toggle
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
